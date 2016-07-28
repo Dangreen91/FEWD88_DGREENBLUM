2783f6d4a753f8e89e2bdbbde87399adfc751000
Dangreen91 <daniel.greenblum@gmail.com>
2016-07-27 21:10:25 -0400

    hw 15

--- Realize Israel/JavaScript/app.js
+++ Realize Israel/JavaScript/app.js
@@ -1,8 +1,12 @@
-$(document).ready(function()
-{          
-    $("li").hover(function(){
-        $("#About").show()},
-        function () {
-        $("#About").hide()
-    });
-});
+$(document).ready(function () {
+	// console.logs the window.width
+	$(window).resize(function () {
+		console.log($(window).width())
+		if($(window).width()>=1024){
+			$('#mobile-nav').hide();
+		}
+	})
+	$('#burger').click(function(){
+		$('#mobile-nav').slideToggle();
+	});
+})
